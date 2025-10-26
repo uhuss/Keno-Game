955a820e1d5365abf543f13023796c5ee27dceb7
agarc428 <agarc428@uic.edu>
2025-10-26 01:31:29 -0500

    Update Player.java
    added neatness

--- Player.java
+++ Player.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
 public class Player {
-    private Integer balance;
-    private Integer currentBet;
-    private List<Integer> selectedNumbers;
-    private Payout payout;
+
+    private Integer balance; //balance for player
+    private Integer currentBet; //wager that player placed
+    private List<Integer> selectedNumbers; //numbers that player chose
+    private Payout payout; //payout particular to player's matched numbers
     
+    //default constructor
     public Player() {
         this.balance = 100;
         this.selectedNumbers = new ArrayList<>();
@@ -15,16 +18,21 @@ public class Player {
         this.payout = null;
     }
     
+    //parameterized constructor (adds balance for recurring games)
     public Player(int addBalance) {
-    	this.balance = addBalance;    	
+    	this.balance = this.balance + addBalance;
     	this.selectedNumbers = new ArrayList<>();
     	this.currentBet = 0;
     	this.payout = null;
 
     }
+    
+    //initalizes payout instance through passing in the player's choice of spot(s) game
     public void initializePayout(int spots) {
     	this.payout = new Payout(spots);
     }
+    
+    //getters
     
     public Integer getBalance() {
     	return balance;
@@ -46,6 +54,7 @@ public class Player {
     	return payout;
     }
     
+    //function for intializing selected numbers and payout 
     public boolean selectNumbers(List<Integer> numbers) {
         if (numbers == null || numbers.size() < 1 || numbers.size() > 10) {
             return false;
@@ -55,7 +64,7 @@ public class Player {
         return true;
     }
     
-    // This is the method the UI will call for "Pick Random Numbers"
+    //function that generates numbers if player chose not to pick themselves
     public void quickPick(int numSpots) {
         Random rand = new Random();
         selectedNumbers.clear();
@@ -70,6 +79,7 @@ public class Player {
         this.payout = new Payout(numSpots);
     }
     
+    //function that checks the vaildity of the bet and decreases the amount from player's balance
     public boolean placeBet(int amount) {
         if (amount <= balance && amount > 0) {
             this.currentBet = amount;
@@ -79,14 +89,17 @@ public class Player {
         return false;
     }
     
+    //function that awards winnings after matched numbers found
     public void awardWinnings(int amount) {
         this.balance += amount;
     }
     
+    //function that returns T/F if balance is less than amount wagered
     public boolean canAffordBet(int amount) {
         return amount <= balance;
     }
     
+    //function that calculates multiplier 
     public int calculateWinnings(int matches) {
     	if(payout == null) {
     		return 0;
